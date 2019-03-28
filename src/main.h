65f816e00de4a127f0cd48837ef729dc58c628a8
Keeton Feavel <me@kfeavel.com>
2019-03-28 14:59:36 -0700

    Attempt to fix git

--- src/main.h
+++ src/main.h
@@ -1,28 +1,26 @@
 struct device_stats {
-	unsigned char word[64];			// found word passed from GPU
-	int hash_found;			// boolean if word is found
+  unsigned char word[32][64]; // found word passed from GPU
+  int hash_found;         // boolean if word is found
 };
 
 struct cuda_device {
-	int device_id;
-	struct cudaDeviceProp prop;
+  int device_id;
+  struct cudaDeviceProp prop;
 
-	int max_threads;
-	int max_blocks;
-	int shared_memory;
+  int max_threads;
+  int max_blocks;
+  int shared_memory;
 
-	void *device_global_memory;
-	int device_global_memory_len;
+  void *device_global_memory;
+  int device_global_memory_len;
 
-	void *host_memory;
+  void *host_memory;
 
-	void *device_stats_memory;
-	struct device_stats stats;
+  void *device_stats_memory;
+  struct device_stats stats;
 
-	unsigned int target_hash[4];
+  unsigned int target_hash[15][4];
 
-	// to be used for debugging
-	void *device_debug_memory;
+  // to be used for debugging
+  void *device_debug_memory;
 };
-
-
