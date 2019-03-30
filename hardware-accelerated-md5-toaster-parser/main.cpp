b6d7e2b52d04981393c5ac0358d9c621cfe5d805
Keeton Feavel <keetonfeavel@gmail.com>
2019-03-30 15:27:22 -0700

    1337 speak generator

--- hardware-accelerated-md5-toaster-parser/main.cpp
+++ hardware-accelerated-md5-toaster-parser/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,42 @@ struct wordlist_file {
   int len;
   char *words;
 };
+
+
+// Function to generate permutations 
+vector<string> permute(string input) {
+    map<char, string> leet = {
+        {'a', "@4"},
+        {'b', "8"},
+        {'e', "3"},
+        {'g', "96"},
+        {'i', "1"},
+        {'l', "1|!"},
+        {'o', "0"},
+        {'s', "5$"},
+        {'t', "7"},
+        {'z', "2"}
+    };
+    vector<string> list;
+    int n = input.length(); 
+    // Number of permutations is 2^n 
+    uint32_t max = 1 << n;
+    // Using all subsequences and permuting them 
+    for (int i = 0; i < max; i++) { 
+        // If j-th bit is set, we convert it to upper case 
+        string combination = input; 
+        for (int j = 0; j < n; j++) {
+            if (((i >> j) & 1) == 1 && (leet.find(input[j]) != leet.end())) {
+                for (int k = 0; (leet[input[j]])[k] != '\0'; k++) {
+                    combination[j] = (leet[input[j]])[k];
+                }
+            }     
+        }
+        list.push_back(combination);
+    } 
+    return list;
+} 
+
 
 map<string, int> read_wordlist(struct wordlist_file *file) {
     map<string, int> mp;
@@ -27,13 +64,17 @@ map<string, int> read_wordlist(struct wordlist_file *file) {
         transform(newWord.begin(), newWord.end(), newWord.begin(), ::tolower);
         if (!mp.count(newWord) && newWord.size() > 3) {
             mp.insert(make_pair(newWord, 1));
-        } else if(newWord.size() > 3) {
-            mp[newWord]++;
+            vector<string> list = permute(newWord);
+            for(int i = 0; i < list.size(); i++) {
+                mp.insert(make_pair(list[i], 1));
+            }
         }
     }
-    printf("The longest word\"%s\" is %i long", lWord.c_str(), len);
+    printf("The longest word \"%s\" is %i long", lWord.c_str(), len);
     return mp;
 }
+
+
 
 void writeMapToFile(map<string, int> mp, const char* fn) {
     ofstream ofs;
@@ -44,6 +85,7 @@ void writeMapToFile(map<string, int> mp, const char* fn) {
 }
 
 int main(int argc, char** argv) {
+
     wordlist_file words;
     words.ifs.open(argv[1]);
     map<string, int> mp;
